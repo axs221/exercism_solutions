e9b8dc15785ac20b08d648bab14ce17ddfff52ba
Shawn Axsom <axs221@gmail.com>
2017-06-09 08:13:09 -0400

    Simplify mod operations, avoid manual casting

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -11,21 +11,17 @@ type Clock struct {
 	hour, minute int
 }
 
-func adjust(amount int, max float64) int {
-	return int(math.Mod(float64(amount), max))
-}
-
 func New(hour, minute int) Clock {
-	hour = adjust(hour+int(minute/60), 24)
-	minute = adjust(minute, 60)
+	hour = (hour + int(minute/60)) % 24
+	minute = minute % 60
 
 	if minute < 0 {
-		minute = adjust(int(math.Abs(float64(60+minute))), 60)
+		minute = (60 + minute) % 60
 		hour -= 1
 	}
 
 	if hour < 0 {
-		hour = adjust(int(math.Abs(float64(24+hour))), 24)
+		hour = (24 + hour) % 24
 	}
 
 	return Clock{hour, minute}
